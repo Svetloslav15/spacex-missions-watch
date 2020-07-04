b903baaa23c2934b6c273949482c6cbdf00af3de
Svetloslav15 <sn_novoselski@abv.bg>
2020-07-04 16:46:09 +0300

    add about page

--- spacex-launcher/src/App.js
+++ spacex-launcher/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-import {Route} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 
 import HomePage from './pages/home/HomePage';
 import LaunchDetails from './pages/launch-details/LaunchDetails';
 import UpcomingLaunches from './pages/upcoming/UpcomingLaunches';
 import PastLaunches from './pages/past/PastLaunches';
+import About from './pages/about/About';
 import Navigation from './common/navigation/Navigation';
 
 
@@ -13,10 +14,13 @@ const App = () => {
   return (
     <div>
       <Navigation/>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/launches/:id' component={LaunchDetails} />
-        <Route exact path='/upcoming' component={UpcomingLaunches} />
-        <Route exact path='/past' component={PastLaunches} />
+        <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route exact path='/upcoming' component={UpcomingLaunches} />
+            <Route exact path='/past' component={PastLaunches} />
+            <Route exact path='/about' component={About} />
+            <Route exact path='/launches/:id' component={LaunchDetails} />
+        </Switch>
     </div>
   );
 };
---
+++ spacex-launcher/src/pages/about/About.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className="bg-none container h-50vh jumbotron mt-5 position-relative">
+            <div className="home-container"/>
+            <div className="front-container text-center p-3">
+                <h2>About</h2>
+                <h4 className="mt-4 text-shadow">This site is not for commercial use, and doesn't have relations to SpaceX. It's
+                    purpose is to show upcoming and past missions of SpaceX.
+                </h4>
+                <h4 className="mt-4 text-shadow">
+                    The site is still in development, will be updated frequently..</h4>
+                <h4 className="mt-4 text-shadow">I will be thankful if you give me a star at github and share it will your friends!</h4>
+                <h4 className="mt-4 text-shadow">
+                    Find me on <a className="link-c font-weight-bold" href='https://github.com/Svetloslav15'>Github</a> or
+                    <a className="link-c font-weight-bold" href='https://novoselski.net/'> My Website</a></h4>
+            </div>
+        </div>
+    );
+};
+
+export default About;
