5f203ea6beffaed0fb6ee29516782a2ef8eb890c
Svetloslav15 <sn_novoselski@abv.bg>
2020-07-04 14:47:59 +0300

    add next launch section

--- spacex-launcher/src/pages/home/HomePage.jsx
+++ spacex-launcher/src/pages/home/HomePage.jsx
@@ -6,6 +6,7 @@ const HomePage = () => {
     const [nextLaunch, setNextLaunch] = useState(null);
     const [timeToNextLaunch, setTimeToNextLaunch] = useState({});
     const [launchPad, setLaunchPad] = useState(null);
+    const [rocket, setRocket] = useState(null);
 
     useEffect(() => {
         if (!nextLaunch) {
@@ -21,6 +22,10 @@ const HomePage = () => {
                 LaunchService.getLaunchPad(nextLaunch.launchpad)
                     .then(data => setLaunchPad(data));
             }
+            if (!rocket) {
+                LaunchService.getRocketById(nextLaunch.rocket)
+                    .then(data => setRocket(data));
+            }
             setInterval(() => {
                 setTimeToNextLaunch(LaunchHelper.calculateTimeBetweenTwoDates(nextLaunch.date_utc));
             }, 1000);
@@ -29,11 +34,36 @@ const HomePage = () => {
     return (
         <div className="bg-none container h-50vh jumbotron mt-5 position-relative">
             <div className="home-container"/>
-            <div className="front-container">
-                <h2>{nextLaunch && nextLaunch.name}</h2>
-                <h2>{launchPad && launchPad.full_name}</h2>
-                <h3>{timeToNextLaunch.days} days : {timeToNextLaunch.hours} hours : {timeToNextLaunch.minutes} minutes : {timeToNextLaunch.seconds} seconds</h3>
-            </div>
+            {nextLaunch ? <div className="front-container">
+                <h2>{nextLaunch.name}</h2>
+                <img className="patch-image" src={nextLaunch.links.patch.small} alt="Patch"/>
+                <p className="py-1">
+                    <span className="pr-4"><i className="fas fa-rocket"/> Rocket: {rocket && rocket.name}</span>
+                    <span className="pl-4">Flight # {nextLaunch.flight_number}</span>
+                </p>
+                <p className="py-1">
+                    <i className="fas fa-map-marker-alt mr-2"/>
+                    {launchPad && launchPad.full_name}
+                </p>
+                <h3 className="mx-auto row justify-content-center">
+                    <div className="column-display">
+                        <span className="font-size-36">{timeToNextLaunch.days}</span>
+                        <span className="font-size-22">DAYS</span>
+                    </div> :
+                    <div className="column-display">
+                        <span className="font-size-36">{timeToNextLaunch.hours}</span>
+                        <span className="font-size-22">HOURS</span>
+                    </div> :
+                    <div className="column-display">
+                        <span className="font-size-36">{timeToNextLaunch.minutes}</span>
+                        <span className="font-size-22">MINUTES</span>
+                    </div> :
+                    <div className="column-display">
+                        <span className="font-size-36">{timeToNextLaunch.seconds}</span>
+                        <span className="font-size-22">SECONDS</span>
+                    </div>
+                </h3>
+            </div> : ''}
         </div>
     );
 };
--- spacex-launcher/src/services/LaunchService.js
+++ spacex-launcher/src/services/LaunchService.js
@@ -1,10 +1,15 @@
 const BASE_URL = 'https://api.spacexdata.com/v4/launches/';
 const BASE_URL_LAUNCHPADS = 'https://api.spacexdata.com/v4/launchpads/';
+const BASE_URL_ROCKETS = 'https://api.spacexdata.com/v4/rockets/';
 
 export default {
     getLaunchPad: (id) => {
-        console.log(id);
         return fetch(`${BASE_URL_LAUNCHPADS}${id}`)
+            .then(data => data.json())
+            .then(data => data);
+    },
+    getRocketById: (id) => {
+        return fetch(`${BASE_URL_ROCKETS}${id}`)
             .then(data => data.json())
             .then(data => data);
     },
